fa810b9256c926a48363386168f877d838148be8
Aymeric-sc <aymericsc05@gmail.com>
2026-05-28 11:44:31 +0200

    Update data_streaming.py

--- data_streaming.py
+++ data_streaming.py
@@ -3,6 +3,16 @@ from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from generateur import Event 
 
+from pyspark.sql import SparkSession
+spark = SparkSession.builder \
+    .appName("StreamingApp") \
+    .master("local[*]") \
+    .config("spark.sql.shuffle.partitions", "4") \
+    .config("spark.driver.memory", "2g") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")    
+
 def getTcpData():
     lines = spark \
     .readStream \
@@ -10,6 +20,3 @@ def getTcpData():
     .option("host", "localhost") \
     .option("port", 9999) \
     .load()
-
-    
-
